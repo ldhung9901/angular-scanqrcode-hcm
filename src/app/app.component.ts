5a07add982611697c4d80cc3cdc4a86644ef9b4a
TECHPRO-MAC <ldhung9901@gmail.com>
2023-11-17 16:14:22 +0700

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,161 +1,83 @@
-import { Component } from "@angular/core";
-import * as moment from "moment";
-import readXlsxFile from "read-excel-file";
-import { hashPassword, zeroPad } from "./utils";
-import writeXlsxFile, { SheetData } from "write-excel-file";
+import { Component } from '@angular/core';
+import { BarcodeFormat } from '@zxing/library';
+import * as moment from 'moment';
+import { BehaviorSubject, Subject } from 'rxjs';
 
-interface Lottery {
-  type: string;
-  ki: string;
-  code: string;
-  createDate: Date;
-  index: string;
-  lastString: string;
-}
 @Component({
-  selector: "my-app",
-  template: `
-    <div style="margin: 20px;">
-    <div>v1.0.0</div>
-      <input
-        type="file"
-        accept=".xlsx,.csv"
-        class="file-upload"
-        (change)="onFileDrop($event.target.files[0])"
-      />
-    </div>
-  `,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  searchTxt: string = "";
-  listData: {
-    A: string;
-    B: string;
-    C: string;
-  }[] = [];
-  cacheListData: {
-    A: string;
-    B: string;
-    C: string;
-  }[] = [];
-  setIdA: Map<string, { A: string; B: string; C: string }> = new Map<
-    string,
-    { A: string; B: string; C: string }
-  >();
-  setIdB: Map<string, { A: string; B: string; C: string }> = new Map<
-    string,
-    { A: string; B: string; C: string }
-  >();
-  constructor() {}
-  getDataRecordsArrayFromCSVFile(csvRecordsArray: string[]) {
-    let csvArr = [];
+  title = 'angular-scan-hcm';
+  private readonly _destroyRef: Subject<void> = new Subject();
+  isConnect: boolean;
+  hasPermission: boolean = false;
 
-    for (let i = 0; i < csvRecordsArray.length - 1; i++) {
-      let currentRecord = (<string>csvRecordsArray[i]).split(",");
-      let csvRecord: any[] = [];
-      csvRecord[0] = currentRecord[0].trim();
-      csvRecord[1] = currentRecord[1].trim();
-      csvRecord[2] = currentRecord[2].trim();
-      csvRecord[3] = currentRecord[3].trim();
-      csvArr.push(csvRecord);
-    }
-    return csvArr;
+  torchEnabled = false;
+  torchAvailable$ = new BehaviorSubject<boolean>(false);
+  enableText: boolean = true;
+  availableDevices: MediaDeviceInfo[];
+  deviceCurrent: MediaDeviceInfo;
+  deviceSelected: string;
+  hasDevices: boolean;
+  tryHarder = false;
+  formatsEnabled: BarcodeFormat[] = [
+    BarcodeFormat.CODE_128,
+    BarcodeFormat.DATA_MATRIX,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.QR_CODE,
+  ];
+  ticketNumber: string = '';
+  dateReward: string = '';
+  ticketType: string = '';
+  kyHieu: string = '';
+
+  constructor() {
+
   }
-  async convertData(data: any[], fileName: string) {
-    let rawData = data;
-    let newData = [];
-    newData = rawData.map((e) => {
-      let code = e[1].toString();
 
-      var dateString = e[2].toString().split("/");
-      if (dateString[0].length === 1) {
-        dateString[0] = zeroPad(0, 1) + dateString[0];
-      }
-      if (dateString[1].length === 1) {
-        dateString[1] = zeroPad(0, 1) + dateString[1];
-      }
-      let next =
-        e[0].toString() +
-        code +
-        dateString[0] +
-        dateString[1] +
-        dateString[2] +
-        zeroPad(e[3], 6);
-      return [
-        e[0],
-        e[1],
-        `${dateString[0]}/${dateString[1]}/${dateString[2]}`,
-        zeroPad(e[3], 6),
-        `https://kqxoso.online/binhphuoc/${next}`,
-        hashPassword(next, 5)
-      ];
-    });
 
-    this.exportToCsv(`${fileName.split('.')[0]}.csv`,newData);
-    // await writeXlsxFile(newData, {
-    //   fileName: `${fileName.split('.')[0]}.xlsx`,
-    // });
+  ngOnInit(): void {
+    console.log(1);
+
   }
-  async onFileDrop(file: File) {
-    let rawData: any[] = [];
-    if (file.type === "text/csv") {
-      let reader = new FileReader();
-      reader.readAsText(file);
-      reader.onload = () => {
-        let csvData = reader.result;
-        let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);
 
-        rawData = this.getDataRecordsArrayFromCSVFile(csvRecordsArray);
-        console.log(rawData);
-      };
-      reader.onloadend = async () => {
-        await this.convertData(rawData, file.name);
-      };
-    } else {
-      rawData = await readXlsxFile(file);
-      await this.convertData(rawData, file.name);
-    }
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
   }
-  exportToCsv(filename: string, rows: object[]) {
-    if (!rows || !rows.length) {
+
+  onTorchCompatible(isCompatible: boolean): void {
+    this.torchAvailable$.next(isCompatible || false);
+  }
+
+  onDeviceChange(device: MediaDeviceInfo) {
+    const selectedStr = device?.deviceId || '';
+    if (this.deviceSelected === selectedStr) {
       return;
     }
-    const separator = ",";
-    const keys = Object.keys(rows[0]);
-    let csvContent =
-      // keys.join(separator) +
-      // "\n" +
-      rows
-        .map((row) => {
-          return keys
-            .map((k) => {
-              let cell = row[k] === null || row[k] === undefined ? "" : row[k];
-              cell =
-                cell instanceof Date
-                  ? cell.toLocaleString()
-                  : cell.toString().replace(/"/g, '""');
-              if (cell.search(/("|,|\n)/g) >= 0) {
-                cell = `"${cell}"`;
-              }
-              return cell;
-            })
-            .join(separator);
-        })
-        .join("\r\n");
-        csvContent = csvContent.concat("\r\n");
+    this.deviceSelected = selectedStr;
+    this.deviceCurrent = device || undefined;
+  }
 
-    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+  onCodeResult(resultString: string) {
+    console.log(resultString);
 
-    const link = document.createElement("a");
-    if (link.download !== undefined) {
-      // Browsers that support HTML5 download attribute
-      const url = URL.createObjectURL(blob);
-      link.setAttribute("href", url);
-      link.setAttribute("download", filename);
-      link.style.visibility = "hidden";
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
+
+    this.ticketNumber = resultString.slice(-6)
+    this.dateReward = moment(resultString.slice(-14, -6), 'DDMMYYYY').format("DD/MM/yyyy")
+
+    this.ticketType = resultString.slice(0, 1)
+    this.kyHieu = resultString.slice(1, 5)
+
+
+    console.log(resultString);
+
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideRouter, RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { routes } from './app.routing';
-import { NzTableModule } from 'ng-zorro-antd/table';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NzInputModule } from 'ng-zorro-antd/input';
-
-registerLocaleData(en);
-
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @NgModule({
-  // import RouterModule for templates (router directives)
-  imports: [BrowserModule
-    , RouterModule,
-    NzTableModule,
-    NzInputModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+  declarations: [
+    AppComponent
   ],
-  // provide Router with routes
-  providers: [provideRouter(routes), { provide: NZ_I18N, useValue: en_US }],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ZXingScannerModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
